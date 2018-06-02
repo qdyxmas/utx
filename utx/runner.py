7843a172c1253b1bda45121ae11e0fad5be27258
qdyxmas <qdyxmas@gmail.com>
2018-06-02 15:42:01 +0800

    Update runner.py

--- utx/runner.py
+++ utx/runner.py
@@ -2,15 +2,20 @@
 # -*- coding: UTF-8 -*-
 import os
 import unittest
+import datetime
 from utx import log
 from utx.BSTestRunner import BSTestRunner
+import coverage
 
+class TestRunner():
 
-class TestRunner:
-
-    def __init__(self):
+    def __init__(self,**kargs):
         self.case_dirs = []
-
+        if not kargs:
+            self.reserved['testName'] = 'qudeyong'
+        else:
+            self.reserved = kargs
+        self.start_time=datetime.datetime.now()
     def add_case_dir(self, dir_path):
         """添加测试用例文件夹，多次调用可以添加多个文件夹，会按照文件夹的添加顺序执行用例
 
@@ -30,18 +35,29 @@ class TestRunner:
             self.case_dirs.append(dir_path)
 
     def run_test(self, report_title='接口自动化测试报告'):
-
+        cov = coverage.Coverage(source=self.case_dirs)
+        cov.start()
         if not self.case_dirs:
             raise Exception("请先调用add_case_dir方法，添加测试用例文件夹")
 
         if not os.path.exists("report"):
             os.mkdir("report")
-
-        report_dir = os.path.abspath("report")
+        dirname = '{}'.format(self.start_time.strftime("%Y-%m-%d-%H-%M-%S"))
+        report_dir = os.path.abspath("report/"+dirname)
+        os.mkdir(report_dir)
+        os.mkdir(report_dir+'/coverage')
         suite = unittest.TestSuite()
         for case_path in self.case_dirs:
-            suite.addTests(unittest.TestLoader().discover(case_path))
-        BSTestRunner(report_dir=report_dir, report_title=report_title).run(suite)
+            # print("case_path=",case_path)
+            suite.addTests(unittest.TestLoader().discover(case_path,pattern='*test.py'))
+        BSTestRunner(report_dir=report_dir, report_title=self.reserved['testName'],**self.reserved).run(suite)
+        # BSTestRunner(report_dir=report_dir, report_title=report_title,**self.reserved).run(suite)
 
         print("测试完成，请查看报告")
-        os.system("start report")
+        os.chdir(report_dir)
+        cov.stop()
+        cov.save()
+        cov.html_report(directory=report_dir+'/coverage')
+        os.system("tar -zcvf test_result.tar.gz ./*")
+        fzip = report_dir+'/test_report.zip'
+        #os.system('sz test_result.tar.gz')
