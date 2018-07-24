b2ba7369ab67edc748bbae857d1baf032fa5b24a
qdyxmas <qdyxmas@gmail.com>
2018-07-24 18:51:15 +0800

    添加report日志目录

--- testrun.py
+++ testrun.py
@@ -11,9 +11,9 @@ rootdir = os.path.join(os.path.dirname(os.path.realpath(__file__)),"xkool_site")
 print("rootdir=",rootdir)
 #os.chdir(rootdir)
 if __name__ == '__main__':
-    kargs =  {'testName':'qdy@xkool.xyz','rootdir':rootdir}
+    kargs =  {'testName':'qdy@xkool.xyz','rootdir':rootdir,'report':'/var/www/html/report'}
     try:
-        options,args = getopt.getopt(sys.argv[1:],"h:",["user=",'rootdir='])
+        options,args = getopt.getopt(sys.argv[1:],"h:",["user=",'rootdir=','report'])
     except getopt.GetoptError:
         print('error')
         sys.exit()
@@ -22,8 +22,10 @@ if __name__ == '__main__':
             kargs['testName'] = value
         elif name in ("--rootdir"):
             kargs['rootdir'] = value
+        elif name in ("--report"):
+            kargs['report'] = value
     log.set_level(logging.DEBUG)
     print(kargs)
-    runner = TestRunner(testName=kargs['testName'])
+    runner = TestRunner(**kargs)
     runner.add_case_dir(kargs['rootdir'])
     runner.run_test()
